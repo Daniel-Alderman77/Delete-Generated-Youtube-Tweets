d53f369020a0ad5af9029cf834d0e3220ea52c2b
Daniel Alderman <daniel.alderman77@gmail.com>
2016-07-25 14:32:03 +0100

    Created DeleteYoutubeTweets. Began to implement get_tweets

--- delete_youtube_tweets.py
+++ delete_youtube_tweets.py
@@ -1,25 +1,45 @@
 # Based upon https://gist.github.com/yanofsky/5436496
 
 import sys
+import tweepy #https://github.com/tweepy/tweepy
 
 
-# Twitter API credentials
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+class DeleteYoutubeTweets:
 
+    def __init__(self):
+        self.name = self
 
-def get_tweets(screen_name):
-    print "Now attempting to retrieve tweets for " + screen_name
+        # Twitter API credentials
+        self.consumer_key = ""
+        self.consumer_secret = ""
+        self.access_key = ""
+        self.access_secret = ""
 
+    def get_tweets(self, screen_name):
+        print "Now attempting to retrieve tweets for " + screen_name
 
-if __name__ == '__main__':
-    try:
-        user_input = sys.argv[1]
+        # authorize twitter, initialize tweepy
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_key, self.access_secret)
+        api = tweepy.API(auth)
 
-        # Pass in the username of the account
-        get_tweets(user_input)
+        # initialize a list to hold all the tweepy Tweets
+        list_of_tweets = []
 
-    except IndexError:
-        print "Please enter twitter username as an argument"
+        # make initial request for most recent tweets (200 is the maximum allowed count)
+        response = api.user_timeline(screen_name=screen_name, count=200)
+
+        # save most recent tweets
+        list_of_tweets.extend(response)
+
+        print list_of_tweets
+
+    if __name__ == '__main__':
+        try:
+            user_input = sys.argv[1]
+
+            # Pass in the username of the account
+            get_tweets(user_input)
+
+        except IndexError:
+            print "Please enter twitter username as an argument"
